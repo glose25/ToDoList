24d337fb76682778ac9810ad5bfa24969001213e
Jason Glose <glose25@gmail.com>
2014-06-05 23:52:11 -0400

    initial commit

---
+++ ToDoList/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ToDoList
+//
+//  Created by Jay Glose on 11/7/13.
+//  Copyright (c) 2013 Jay Glose. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
